4c51d5e1ff9ade4f9fee96a0e1d12fcd0088f5f6
Eduardo Baia <eduardobaia@Eduardos-Air.Home>
2022-06-25 15:17:10 +0100

    layout and input functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'teamapp';
+
+  newMemberName = '';
+
+  members: string[] = [];
+  errorMessage='';
+
+  onInput(member: string ){
+    this.newMemberName= member;
+    console.log(member)
+  }
+
+addMember(){
+
+  if(!this.newMemberName){
+  this.errorMessage="Name can't be empty.";
+    return;
+  }
+  this.members.push(this.newMemberName);
+  this.newMemberName='';
+  this.errorMessage='';
+
+}
+
+
 }

ef0e59b7df613ec3c32137ab28e5a6131b83cd77
Eduardo Baia <eduardobaia@Eduardos-Air.Home>
2022-06-25 16:15:33 +0100

    logical splid and divise team

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,17 @@ export class AppComponent {
 
   members: string[] = [];
   errorMessage='';
+  numberOfTeams: number | '' = '';
+  teams: string[][]= [];
 
   onInput(member: string ){
     this.newMemberName= member;
     console.log(member)
   }
 
+  onNumberOfTeamsInput(value: string){
+    this.numberOfTeams= Number(value);
+  }
 addMember(){
 
   if(!this.newMemberName){
@@ -29,5 +34,35 @@ addMember(){
 
 }
 
+generateTeams(){
+  console.log("entrou em generate")
 
+  if(!this.numberOfTeams ||  this.numberOfTeams <= 0){
+    console.log("entrou em if de retorni ´")
+    return;
+  }
+  const allMembers = [...this.members];
+
+  console.log("numbes of temas ", this.numberOfTeams)
+
+  while(allMembers.length){
+    for(let i = 0; i < this.numberOfTeams; i++ ){
+      console.log("entrou em fooor ")
+      const randomIndex = Math.floor(Math.random() * allMembers.length);
+      // splice getting the position of the item and remove it from the list
+      const member = allMembers.splice(randomIndex,1)[0];
+
+      //if member are null, break, do not add.
+      if(!member)break;
+      //if teams are not empty it add new member, if no  add a first member inside array
+      if(this.teams[i]){
+        this.teams[i].push(member);
+      }else{
+        this.teams[i] = [member];
+      }
+
+    }
+  }
+  console.log(this.teams)
+}
 }
